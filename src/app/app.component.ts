61f9184c0a9a650e81f0731269d5f4886d78c06e
Sanjay Samantra <sanjaysamantra1@gmail.com>
2022-10-13 21:41:37 +0530

    13 october

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_august_2022_2';
+  title = 'angular_august_2022_2nd';
 }
